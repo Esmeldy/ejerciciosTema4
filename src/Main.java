d24472d2a127a20086eccac07be32dac558330fa
xXEsmeldyXx <esmeldyfm@hotmail.com>
2022-07-25 23:37:44 +0200

    Hecho el punto 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+  //      NumeroIf(-2);
+    }
+
+    /**
+     * Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+     * Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
+     */
+    public static void NumeroIf (int num){
+
+        if (num == 0){
+            System.out.println("Es igual a 0");
+        } else if (num < 0) {
+            System.out.println("Es negativo");
+        }
+        else {
+            System.out.println("Es positivo");
+        }
+
+
     }
 }
